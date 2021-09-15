7230855ec8e760838f421b8b5c970ab4e47b3749
NevidX <433android@mail.ru>
2021-09-15 13:35:52 +0300

    Added qestions section with resposive

--- js/script.js
+++ js/script.js
@@ -113,3 +113,65 @@ class Slider {
 	}
 }
 const slider = new Slider("slider1", "slider__slide", "left", "right");
+
+
+class SpoilerGroup {
+	#contentWidth = [];
+	#trigger;
+	#body;
+	#content;
+	#accordeonType;
+	#style;
+	constructor(spoilerTrigger, spoilerBody, spoilerContent, accordeonType, style) {
+		this.#trigger = document.querySelectorAll(spoilerTrigger);
+		this.#body = document.querySelectorAll(spoilerBody);
+		this.#content = spoilerContent;
+		this.#accordeonType = accordeonType || "none";
+		this.#style = style || "none";
+		this.init();
+	}
+	/*initialize spoilers (give them height, close them, check accordeon type)*/
+	init() {
+		this.#contentWidth = [];
+		this.#trigger.forEach((item, i) => {
+			this.#contentWidth.push(this.#body[i].offsetHeight + "px");
+			this.#body[i].style.maxHeight = "0"; //comment this if you want spoilers to be opened
+			if (this.#accordeonType === "open") {
+				this.#body[0].style.maxHeight = this.#contentWidth[0];
+			}
+			item.addEventListener("click", () => {
+				if (!(this.#style === "none")) {
+					item.classList.toggle(this.#style);
+				}
+
+				if (this.#accordeonType === "open") {
+					this.close();
+				}
+				if (this.#body[i].style.maxHeight === "0px") {
+					if (this.#accordeonType === "close") {
+						this.close();
+					}
+					this.#body[i].style.maxHeight = this.#contentWidth[i];
+				} else {
+					this.#body[i].style.maxHeight = "0";
+				}
+			});
+			window.addEventListener("resize", () => {
+				this.recalc();
+			})
+		});
+	}
+	/*recalc height of elements*/
+	recalc() {
+		this.#body.forEach((item, i) => {
+			this.#contentWidth[i] = item.querySelector(this.#content).offsetHeight + "px";
+		})
+	}
+	/*close all spoilers*/
+	close() {
+		this.#body.forEach((item, i) => {
+			this.#body[i].style.maxHeight = "0";
+		})
+	}
+}
+const mySpoilerGroup = new SpoilerGroup(".spoiler__trigger", ".spoiler__body", ".spoiler__content", "none", "spoiler__arrow-active");
