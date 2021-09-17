def70a83f50a86ea427199fca8ce9a4b2352ca25
NevidX <433android@mail.ru>
2021-09-17 10:39:17 +0300

    Problem with spoilers

--- js/spoiler.js
+++ js/spoiler.js
@@ -1,30 +1,44 @@
 
 const spoilerTrigger = document.querySelectorAll(".spoiler__trigger");
 const spoilerBody = document.querySelectorAll(".spoiler__body");
-const content = document.querySelectorAll(".spoiler__content");
 const spoilerStyle = "spoiler__arrow-active" || "none";
+const spoilerBodyStyle = "spoiler__body-active";
 const spoilerContentWidth = [];
 
+const spoilerHeader = (document.querySelector(".spoiler__trigger").parentNode).nextElementSibling;
+spoilerTrigger.forEach((item) => {
+	console.log((item.parentNode).nextElementSibling);
+})
 
 spoilerTrigger.forEach((item, i) => {
 	spoilerContentWidth.push(spoilerBody[i].offsetHeight + "px");
-	spoilerBody.forEach((item, i) => {
-		spoilerBody[i].style.maxHeight = "0";
-	})
+
 	item.addEventListener("click", () => {
+		let exactBody = (item.parentNode).nextElementSibling;
+		console.log(exactBody);
 		if (!(spoilerStyle === "none")) {
 			item.classList.toggle(spoilerStyle);
 		}
-
-		if (spoilerBody[i].style.maxHeight === "0px") {
-			spoilerBody[i].style.maxHeight = spoilerContentWidth[i];
+		if (!(exactBody.style.maxHeight === "0")) {
+			console.log(exactBody.style.maxHeight);
+			exactBody.style.maxHeight = "100%"
 		} else {
-			spoilerBody[i].style.maxHeight = "0px";
+			exactBody.style.maxHeight = "0"
 		}
+
+
 	});
 	window.addEventListener("resize", () => {
-		spoilerBody.forEach((item, i) => {
-			spoilerContentWidth[i] = item.querySelector(content).offsetHeight + "px";
-		})
+		resize();
 	})
 });
+function resize() {
+	spoilerBody.forEach((item, i) => {
+		spoilerContentWidth[i] = item.querySelector(".spoiler__content").offsetHeight + "px";
+	})
+}
+function close() {
+	spoilerBody.forEach((item, i) => {
+		spoilerBody[i].style.maxHeight = "0";
+	})
+}
